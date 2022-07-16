8a2d30ee8863ddc1a1e88cdb1c2c10c68c83a369
oxidetor <prateek.girdhar@outlook.com>
2022-07-15 23:01:48 -0700

    Improve recursive method and add alt one-liner

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -9,11 +9,16 @@ def fibs(n)
   end
 end
 
-def fibs_rec(n, result = [])
-  return (0...n).to_a if n in (1..2)
+def fibs_rec(n)
+  return *(0...n) if n in (1..2)
 
-  fibs_rec(n - 1, result) << fibs_rec(n - 1, result).last + fibs_rec(n - 2, result).last
+  fibs_rec(n - 1) << fibs_rec(n - 1).last(2).sum
+end
+
+def fibs_ol(n)
+  (1..2).include?(n) ? (0...n).to_a : fibs_ol(n - 1) << fibs_ol(n - 1).last(2).sum
 end
 
 p fibs(8)
 p fibs_rec(8)
+p fibs_ol(8)
